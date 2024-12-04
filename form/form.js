ee5dfc723e21412c0481149a674c983fc9a8f8ab
Bencso <158219939+bencso@users.noreply.github.com>
2024-12-04 09:41:46 +0100

    Refactored form component, added login & register functionality

--- form/form.js
+++ form/form.js
@@ -13,148 +13,147 @@ export class Form {
     }
   }
 
-  render() {
-    const form = document.createElement("form");
-    form.id = this.id;
-    form.method = this.method;
-    form.action = this.action;
-
-    const buttonRow = this.createButtonRow();
+  clear() {
+    const form = document.getElementById(this.id);
+    form.reset();
     this.children.forEach((child) => {
-      if (child instanceof Button) {
-        buttonRow.append(child.render());
-      } else {
-        form.append(child.render());
-      }
+      child.clearState();
     });
-
-    form.appendChild(buttonRow);
-    this.addThemeClass(form);
-    document.body.appendChild(form);
   }
 
-  createButtonRow() {
+  render() {
+    const form = document.createElement("form");
     const buttonRow = document.createElement("div");
     buttonRow.classList.add("button-row");
     buttonRow.style.display = "flex";
     buttonRow.style.flexDirection = "row";
     buttonRow.style.gap = "10px";
-    return buttonRow;
+    form.id = this.id;
+    form.method = this.method;
+    form.action = this.action;
+    this.children.forEach((child) => {
+      if (child instanceof Button) {
+        buttonRow.appendChild(child.render());
+      } else {
+        form.appendChild(child.render());
+      }
+    });
+    form.appendChild(buttonRow);
+    this.addThemeClass(form);
+    document.body.appendChild(form);
   }
 }
 
-//TODO: More attributes for input
 export class Input {
   constructor(type = "text", id, stateName = "value", stateValue = "") {
+    this.stateName = stateName;
+    this.state = {
+      [stateName]: stateValue
+    };
     this.id = id || `input${Math.random().toString(36).substring(2, 11)}`;
     this.type = type;
-    this.stateName = stateName;
-    this.state = { [stateName]: stateValue };
   }
 
   setState(newState) {
-    this.state = { ...this.state, ...newState };
+    this.state = {
+      ...this.state,
+      ...newState
+    };
+    console.error(this.state);
   }
 
   clearState() {
-    this.setState({ [this.stateName]: "" });
-    const input = document.getElementById(this.id);
-    if (input) {
-      input.value = "";
-    }
+    this.setState({
+      [this.stateName]: ""
+    });
   }
 
   getState() {
-    return this.state[this.stateName];
+    return this.state;
   }
 
-  render() {
+  renderEmail() {
+    return new EmailInput(this.id).render();
+  }
+
+  renderDefault() {
     const label = document.createElement("label");
+    const input = document.createElement("input");
     label.htmlFor = this.id;
     label.innerText = this.id;
-    const input = document.createElement("input");
+    label.appendChild(input);
     input.type = this.type;
     input.id = this.id;
     input.name = this.stateName;
+    input.state = this.state;
     input.value = this.state[this.stateName];
-    input.oninput = this.onChange;
-
-    label.appendChild(input);
+    input.addEventListener("input", this.onChange);
     return label;
   }
 
+  render() {
+    switch (this.type) {
+      case "email":
+        return this.renderEmail();
+      default:
+        return this.renderDefault();
+    }
+  }
+
   onChange = (event) => {
-    this.setState({ [this.stateName]: event.target.value });
+    this.setState({
+      [this.stateName]: event.target.value
+    });
   };
 }
-export class CustomEmailInput extends Input {
-  constructor(id, stateName = this.stateName, stateValue = this.stateValue) {
-    super("custom-email", id, stateName, stateValue);
-    this.state[stateName] = { name: "", domain: "", email: "" };
+
+class EmailInput extends Input {
+  constructor(id) {
+    super("email", id);
+    this.state = {
+      username: "",
+      domain: "",
+      value: ""
+    };
+  }
+
+  onChange = (event) => {
+    if (event.target.id.includes("username")) {
+      this.setState({
+        username: event.target.value
+      });
+    }
+    if (event.target.id.includes("domain")) {
+      this.setState({
+        domain: event.target.value
+      });
+    }
+    this.setState({
+      [this.stateName]: `${this.state.username}@${this.state.domain}`
+    });
   }
 
   render() {
     const label = document.createElement("label");
+    const username = document.createElement("input");
+    const domain = document.createElement("input");
     label.htmlFor = this.id;
-    label.innerText = this.stateName;
-
-    const container = document.createElement("div");
-    container.style.display = "flex";
-    container.style.alignItems = "center";
-
-    const nameInput = this.createInput(
-      `${this.id}-name`,
-      `${this.stateName}-name`,
-      this.state[this.stateName].name,
-      this.onNameChange
-    );
-    const domainInput = this.createInput(
-      `${this.id}-domain`,
-      `${this.stateName}-domain`,
-      this.state[this.stateName].domain,
-      this.onDomainChange
-    );
-
-    container.appendChild(nameInput);
-    container.appendChild(document.createTextNode("@"));
-    container.appendChild(domainInput);
-
-    label.appendChild(container);
+    label.innerText = this.id;
+    username.type = "text";
+    username.id = `${this.id}-username`;
+    username.name = "username";
+    username.addEventListener("input", this.onChange);
+    username.value = this.state.username;
+    username.state = this.state;
+    domain.type = "text";
+    domain.id = `${this.id}-domain`;
+    domain.name = "domain";
+    domain.addEventListener("input", this.onChange);
+    domain.value = this.state.domain;
+    domain.state = this.state;
+    label.appendChild(username);
+    label.appendChild(domain);
     return label;
-  }
-
-  createInput(id, name, value, onChange) {
-    const input = document.createElement("input");
-    input.type = "text";
-    input.id = id;
-    input.name = name;
-    input.value = value;
-    input.oninput = onChange;
-    return input;
-  }
-
-  onNameChange = (event) => {
-    const name = event.target.value;
-    const email = `${name}@${this.state[this.stateName].domain}`;
-    this.setState({
-      [this.stateName]: { ...this.state[this.stateName], name, email },
-    });
-  };
-
-  onDomainChange = (event) => {
-    const domain = event.target.value;
-    const email = `${this.state[this.stateName].name}@${domain}`;
-    this.setState({
-      [this.stateName]: { ...this.state[this.stateName], domain, email },
-    });
-  };
-
-  getState() {
-    return this.state[this.stateName];
-  }
-
-  clearState() {
-    this.setState({ [this.stateName]: { name: "", domain: "", email: "" } });
   }
 }
 
@@ -169,7 +168,7 @@ export class Button {
     const button = document.createElement("button");
     button.id = this.id;
     button.innerText = this.label;
-    button.onclick = this.onClick;
+    button.addEventListener("click", this.onClick);
     return button;
   }
 }
